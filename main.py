2d9e5c30ce335cd1d7ca72405497ff72433c5979
Gabriel Mango <gabrielhmango@gmail.com>
2023-09-01 14:41:33 -0300

    Função gerar pessoa

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from time import sleep
+from pprint import pprint
 
 service = ChromeService(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
@@ -9,6 +14,33 @@ driver.implicitly_wait(2)
 
 URL = 'https://geradornv.com.br/gerador-pessoas/'
 
+dados_possoa = {}
+
 driver.get(URL)
-sleep(2)
+sleep(1)
+
+def gerar_pessoa(driver):
+
+    labels = driver.find_elements(By.TAG_NAME, 'label')
+    ps = driver.find_elements(By.TAG_NAME, 'p')
+
+    combinacao = zip(ps[2:28], labels[1:])
+
+    info_pessoa = {}
+
+    for key, value in combinacao:
+        key_str = key.text
+        value_str = value.text
+        info_pessoa[key_str] = value_str
+        
+    return info_pessoa
+
+pessoas = []
+
+for _ in range(100):
+    driver.find_element(By.ID, 'nv-new-generator-people').click()
+    pessoas.append(gerar_pessoa(driver))
+    
+pprint(pessoas)
+
 driver.quit()
